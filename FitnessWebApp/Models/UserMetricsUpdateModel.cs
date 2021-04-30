311210db822eff0a7baf81890aebb30bc25242fb
Andey-gh <korowai98.ag@gmail.com>
2021-04-30 18:01:04 +0300

    FixedMetrics

--- FitnessWebApp/Models/UserMetricsUpdateModel.cs
+++ FitnessWebApp/Models/UserMetricsUpdateModel.cs
@@ -5,8 +5,30 @@ using System.Threading.Tasks;
 
 namespace FitnessWebApp.Models
 {
-    public class UserMetricsUpdateModel:MetricsModel
+    public class UserMetricsUpdateModel
     {
         public string Name { get; set; }
+       
+        public int MetricAge { get; set; }
+
+      
+        public string MetricGoal { get; set; }
+
+        
+        public List<HealthProblem> healthProblems { get; set; }
+
+      
+        public int MetricHeight { get; set; }
+
+        
+        public int MetricPullUps { get; set; }
+
+        
+        public int MetricPushUps { get; set; }
+
+       
+        public int MetricWeight { get; set; }
+       
+        public string MetricGender { get; set; }
     }
 }
